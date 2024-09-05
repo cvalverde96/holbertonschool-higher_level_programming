2abc9f938c27de9fb45d9d9e11335dd8ceb66814
cvalverde96 <cvalverde96@gmail.com>
2024-09-05 11:12:05 -0400

    tenia un typo en respuesta de ejercicio 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
 if last > 5:
-    print("Last digif of", number, "is", last, "and is greater than 5")
+    print("Last digit of", number, "is", last, "and is greater than 5")
 if last == 0:
     print("Last digit of", number, "is", last, "and is 0")
 if last < 6 and last != 0:
-    print("Last digif of", number, "is", last, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", last, "and is less than 6 and not 0")
