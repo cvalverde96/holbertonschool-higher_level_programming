6b815d4e2b307df10ab1f234beb4184474aced60
cvalverde96 <cvalverde96@gmail.com>
2024-09-05 14:26:55 -0400

    subiendo ejercicio numero 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            upper_char = char(ord(char) - 32)
+        else:
+            uppser_char = char
+        print("{}".format(upper_char), end="")
+    print()
