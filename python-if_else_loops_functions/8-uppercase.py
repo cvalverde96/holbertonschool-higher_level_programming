2a5b3343d3775d09e5b7e275994d575cc5b42ad5
cvalverde96 <cvalverde96@gmail.com>
2024-09-05 14:29:30 -0400

    subiendo 8 porque tenia un typo

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
-            upper_char = char(ord(char) - 32)
+            upper_char = chr(ord(char) - 32)
         else:
             uppser_char = char
         print("{}".format(upper_char), end="")
