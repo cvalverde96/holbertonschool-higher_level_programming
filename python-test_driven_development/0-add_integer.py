83c7f9c95fbc0caf07b3aa0c5e53d56b0ec0f44b
cvalverde96 <cvalverde96@gmail.com>
2024-09-12 14:38:12 -0400

    subiendo el 0 con documentacion

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    anade dos integers o floats
+    tenemos isinstance para validar que en efecto sea in o float
+    de no ser asi, va raise el error correcto con su mensaje adecuado
+
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
