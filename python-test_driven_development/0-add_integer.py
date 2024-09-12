54d53801e2eab50250e20b6eda18e7d9c9b561f3
cvalverde96 <cvalverde96@gmail.com>
2024-09-12 14:41:46 -0400

    subeindo 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,6 +5,15 @@ def add_integer(a, b=98):
     tenemos isinstance para validar que en efecto sea in o float
     de no ser asi, va raise el error correcto con su mensaje adecuado
 
+    Args:
+        a: primer numero
+        b: segundo numero, le pusimos default 98
+
+    Returns:
+        retorna resultado de suma
+
+    Raises:
+        TypeError: si a o b no son integers o float
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
