8951b4a77ed3cd392c9d9bad7cfdb398f0906309
cvalverde96 <cvalverde96@gmail.com>
2024-09-16 16:18:50 -0400

    subiendo ejercicio 0 con comentarios

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+this module defines a class called square
+that defines a method called cuadrito
+that returns a {}
+"""
+
 
 class Square:
-    def cuadrito(self):
-        return("{}")
 
+    """
+    Args:
+        self: first parameter
+    Returns:
+        returns {}
+    """
+    def cuadrito(self):
+        return ("{}")
