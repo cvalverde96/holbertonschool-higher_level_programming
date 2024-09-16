83257fc164676a0bf4e6206d9f04e2b9390bfd4c
cvalverde96 <cvalverde96@gmail.com>
2024-09-16 17:31:58 -0400

    subiendo ejercicio 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+this module has a class that defines a square
+"""
+
+
+class Square:
+    """
+    we create a class called square
+    we initialize a private attribute called __size
+    """
+    def __init__(self, size):
+        self.__size = size
