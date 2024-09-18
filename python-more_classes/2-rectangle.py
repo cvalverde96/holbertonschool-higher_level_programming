9d49a637d898c4a05fd47968cce97aa583f6b5e9
cvalverde96 <cvalverde96@gmail.com>
2024-09-18 13:06:52 -0400

    subiendo el 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+modulo que calcula un rectangulo
+"""
+
+
+class Rectangle():
+
+    """
+    clase Rectangulo con dos atributos de instancias
+    privadas de width y height
+    con dos public instance de area y perimeter
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("idth must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        h1 = self.__height + self.__height
+        w1 = self.__width + self.width
+        return (h1 + w1)
