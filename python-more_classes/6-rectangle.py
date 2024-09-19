49c40098f31b6dbe59fc37aa9d8139caf763469a
cvalverde96 <cvalverde96@gmail.com>
2024-09-19 10:56:28 -0400

    subiendo 6 otra vez porque no asigne value bien

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle():
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -39,7 +39,7 @@ class Rectangle():
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
 
     def area(self):
         return (self.__width * self.__height)
