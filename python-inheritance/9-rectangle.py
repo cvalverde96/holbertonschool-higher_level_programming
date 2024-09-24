23e00d2cabdbae912d3ac309909264206ef8565c
cvalverde96 <cvalverde96@gmail.com>
2024-09-24 14:47:26 -0400

    subiendo el 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -6,16 +6,15 @@ modulo que define rectangulo que hereda de baseGeometry
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
 class Rectangle(BaseGeometry):
     """
     inicializamos Rectangle con width y height
 
     funcion que define area y funcion con return de string
     """
-    def __init_(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
