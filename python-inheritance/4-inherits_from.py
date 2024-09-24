2f915e921556a4d2cdbc4e807a4883b76fb8597a
cvalverde96 <cvalverde96@gmail.com>
2024-09-24 10:16:05 -0400

    subiendo el numero 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,24 @@
+#!/usr/bin/class
+
+"""
+modulo contiene funcion que chequea si objeto es instancia
+de una clase que heredo de una clase
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Args:
+        obj: objeto a ser validado
+        a_class: clase a validar herencia
+
+    Returns:
+        True: cierto si objeto es instancia de una clase
+        que heredo de a_class
+        False: falso si no
+    """
+    if isinstance(obj, a_class):
+        if type(obj) is not a_class:
+            return (True)
+    else:
+        return (False)
