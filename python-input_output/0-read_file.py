98cbc51ee385570dace38c91e45be4e46e5dba12
cvalverde96 <cvalverde96@gmail.com>
2024-09-30 12:07:15 -0400

    subeindo el 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
+"""
+function that reads a text file and prints it to stdout
+"""
+
+
 def read_file(filename=""):
+
+    """
+    Args:
+        filename (str, optional): the name of te file to read
+    """
+
     with open(filename, 'r', encoding='utf-8') as f:
         read_data = f.read()
         print(read_data, end='')
