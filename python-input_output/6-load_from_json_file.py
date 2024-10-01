66a6473fbb5cadedb07b12efd3eba64192b38b43
cvalverde96 <cvalverde96@gmail.com>
2024-10-01 10:53:37 -0400

    subiendo ejercicio 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+function that creates an Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+
+    """
+    Args:
+        filename (str): The name of the file containing the JSON
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        my_obj = json.load(file)
+        print(my_obj)
