2acd70f7ae591b39040e3faf6c007656997a8cde
cvalverde96 <cvalverde96@gmail.com>
2024-10-16 14:25:22 -0400

    subiendo el 4 con arreglos

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 
-"""
-part 1
-Setting Up Flask
-"""
-# importando Flask
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
 
-# instantiating un servidor web de Flask
 app = Flask(__name__)
 
-"""
-part 2
-Creating Your First Endpoint
-"""
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+}
 
 
 @app.route("/")
@@ -23,27 +15,9 @@ def home():
     return ("Welcome to the Flask API!")
 
 
-if __name__ == "__main__":
-    app.run()
-
-"""
-part 3
-Serving JSON Data
-"""
-
-
-users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
-
-
 @app.route("/data")
-def usernames():
-    return (jsonify(users))
-
-
-"""
-part 4
-Expanding Your API
-"""
+def getting_usernames():
+    return (jsonify(list(users.keys())))
 
 
 @app.route("/status")
@@ -60,25 +34,22 @@ def full_object(username):
         return (jsonify({"error": "User not found"}), 404)
 
 
-"""
-part 5
-Handling POST Requests
-"""
-
-
 @app.route("/add_user", methods=["POST"])
 def add_user():
     new_user = request.get_json()
+    
+    if "username" not in new_user:
+        return (jsonify({"error": "Username is required"}), 400)
+    
+    username = new_user("username")
+    
+    users[username] = new_user
+    
+    return (jsonify({
+        "message": "User added",
+        "user": new_user
+    }), 201)
 
-    username = new_user.get("username")
 
-    if username in users:
-        return (jsonify({"error": "User already exists"}), 400)
-
-    users[username] = {
-        "name": new_user.get("name"),
-        "age": new_user.get("age"),
-        "city": new_user.get("city")
-    }
-
-    return (jsonify({"message": "User added", "user": users[username]}))
+if __name__ == "__main__":
+    app.run(debug=True)
