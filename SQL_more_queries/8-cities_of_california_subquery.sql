49ce311042ec88b7e1b6a17e860270b5563da4b9
cvalverde96 <cvalverde96@gmail.com>
2024-10-23 14:07:05 -0400

    arreglando el ejercicio numero 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,7 @@
 -- creando un script que imprime lista de todas las ciudades de california
-SELECT @california_id := id FROM states WHERE name = 'California';
-
-SELECT id, name FROM cities WHERE state_id = @california_id ORDER BY id ASC;
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY id ASC;
