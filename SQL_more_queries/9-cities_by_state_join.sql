9aa5ac0c6c7d1cfb88cfd8efc8b8f039ffc78f60
cvalverde96 <cvalverde96@gmail.com>
2024-10-23 14:35:40 -0400

    subiendo el ejercicio numero 9

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- script que imprime todas las ciudades contendias en la base de datos
+SELECT cities.id, cities.name,(
+    SELECT states.name FROM states WHERE states.id = cities.state_id
+) AS state_name
+FROM cities
+ORDER BY cities.id ASC;
