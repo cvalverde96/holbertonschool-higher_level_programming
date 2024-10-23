5e5c6991819d6f66fe02d469bb9b703e272b8990
cvalverde96 <cvalverde96@gmail.com>
2024-10-23 10:28:15 -0400

    arreglando ejercicio numero 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
 -- script que crea un usuario 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
