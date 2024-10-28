5f9c4e46f6dc7213e80bec9fe8d424988b41bc9a
cvalverde96 <cvalverde96@gmail.com>
2024-10-28 15:51:00 -0400

    subiendo file 0 con pycodestily compliance

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,13 +16,13 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
         )
-    
+
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    
+
     cur.close()
     db.close()
