2aa2479513fd116e8e2f365087fad37b1ab540f7
cvalverde96 <cvalverde96@gmail.com>
2024-10-29 14:08:31 -0400

    subiendo model con comentarios

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,6 +13,10 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Clase State que hereda de Base
+    tiene links al MySql table states
+    """
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
