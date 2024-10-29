c1495d2224c3305d0a4e1f90fdd5935f8f805aa6
cvalverde96 <cvalverde96@gmail.com>
2024-10-29 14:05:31 -0400

    subiendo el ejercicio numero 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+a python file that contains the class definition
+of a State and an instance Base = declarative_base()
+"""
+
+import sys
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{db_name}",
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
