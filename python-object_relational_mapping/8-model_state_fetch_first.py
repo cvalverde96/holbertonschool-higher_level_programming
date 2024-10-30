a6a99b0988d0df8706287c4c7361abf72bd84f60
cvalverde96 <cvalverde96@gmail.com>
2024-10-30 09:06:53 -0400

    subiendo ejercicio numero 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+
+    if state:
+        print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
