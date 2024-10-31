bd438569e9f3abb2c9c2860c544846502d675aec
cvalverde96 <cvalverde96@gmail.com>
2024-10-31 09:48:19 -0400

    modificando query para que name like binary n

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
 
     rows = cur.fetchall()
     for row in rows:
