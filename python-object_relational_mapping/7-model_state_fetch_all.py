1e7a7cc2e53083b05eabd988b2d959b8fcf343c7
cvalverde96 <cvalverde96@gmail.com>
2024-10-31 10:20:19 -0400

    el if __name__ le faltaba __

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-if __name__ == "main":
+if __name__ == "__main__":
 
     username = sys.argv[1]
     password = sys.argv[2]
